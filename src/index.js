4ee5970b9bb0e4cc899312b1ccfb41b65a51cbfd
Yawn93 <89013752+Yawn93@users.noreply.github.com>
2024-02-07 22:50:52 +0000

    small fix in js code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-let apiKey = "b8453383f75c0afafd7ft88f33789o5a";
-let inputElement = document.querySelector("#input");
-let context = `user instructions are: generate a poem about ${inputElement.value}, they need to be in PT/PT not PT/BR. Please sign with ShecodesAI at the end of the poem. Make sure to not repeat the same poem. I want four lines: one verse per line.`;
-let prompt = "generate a four-line poem in Portuguese in basic HTML, I want one verse line and then, <br>, and then the other. Make sure to follow the user instructions.";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-
 function generatePoem(event) {
     event.preventDefault();
 
@@ -15,8 +8,15 @@ function generatePoem(event) {
     loadingElement.style.display = "block";
     poemElement.style.display = "none";
 
+    let apiKey = "b8453383f75c0afafd7ft88f33789o5a";
+    let inputValue = document.getElementById("input-prompt").value;
+    let prompt = `generate a four-line poem in PT/PT about ${inputValue}. I want one verse per line.`;
+    let context = "make sure you write one poem-verse per line. It needs to have four lines of text. Use the <br> elements to separate the lines of the poem.";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
     axios.get(apiUrl).then((response) => {
-        console.log(response); 
+
+        console.log(prompt);
         poemElement.innerHTML = response.data.answer;
         loadingElement.style.display = "none";
         poemElement.style.display = "block";
@@ -31,5 +31,5 @@ function generatePoem(event) {
     });
 }
 
-let formElement = document.querySelector("form");
+let formElement = document.querySelector("#form");
 formElement.addEventListener("submit", generatePoem);
