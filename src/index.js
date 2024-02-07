5314164b0428f44ca790be56ceedfa99598ba459
Yawn93 <89013752+Yawn93@users.noreply.github.com>
2024-02-07 19:45:53 +0000

    api prompt engineering

--- src/index.js
+++ src/index.js
@@ -1,33 +1,35 @@
 let apiKey = "b8453383f75c0afafd7ft88f33789o5a";
 let inputElement = document.querySelector("#input");
-let context = `generate poems about ${inputElement.value}, they need to be in PT/PT not PT/BR. Please sign with ShecodesAI at the end of the poem`;
-let prompt = "generate poems in Portuguese";
+let context = `user instructions are: generate a poem about ${inputElement.value}, they need to be in PT/PT not PT/BR. Please sign with ShecodesAI at the end of the poem. Make sure to not repeat the same poem. I want four lines: one verse per line.`;
+let prompt = "generate a four-line poem in Portuguese in basic HTML, I want one verse line and then, <br>, and then the other. Make sure to follow the user instructions.";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-function showAnswer() {
-    let buttonElement = document.querySelector("#submit");
+
+function generatePoem(event) {
+    event.preventDefault();
+
     let poemElement = document.querySelector("#poem");
-    let loading = document.querySelector("#loading");
-  
-    buttonElement.addEventListener("click", () => {
-      loading.innerHTML = "Your poem is being generated... ⌛";
-      loading.style.display = "block";
-      poemElement.style.display = "none";
-      
-      axios.get(apiUrl).then((response) => {
+    let loadingElement = document.querySelector("#loading");
+
+    loadingElement.innerHTML = "Your poem is being generated... ⌛";
+    loadingElement.style.display = "block";
+    poemElement.style.display = "none";
+
+    axios.get(apiUrl).then((response) => {
+        console.log(response); 
         poemElement.innerHTML = response.data.answer;
-        loading.style.display = "none";
+        loadingElement.style.display = "none";
         poemElement.style.display = "block";
-  
+
         new Typewriter("#poem", {
-          strings: [response.data.answer],
-          autoStart: true,
-          pauseFor: 100000,
-          cursor: "",
-          delay: 50,
+            strings: [response.data.answer],
+            autoStart: true,
+            pauseFor: 100000,
+            cursor: "",
+            delay: 50,
         });
-      });
     });
-  }
-  
-  showAnswer();
+}
+
+let formElement = document.querySelector("form");
+formElement.addEventListener("submit", generatePoem);
